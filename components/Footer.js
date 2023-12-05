65c75fdebce4aae69b85c339d491a5c76f9f4d38
Matthew Kivett <mkivett@gmail.com>
2023-12-05 04:18:56 -0600

    Content: Updates to content and layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,18 @@
 export default function Footer() {
   return (
     <>
-      <footer>
+      <section className="l-footer">
+      <footer className="container">
+        References Available On Request
+
+        <img
+          src="/stephen-parker--portrait.svg"
+          alt="Stephen L Parker, DDS"
+          className="portrait"
+        />
 
       </footer>
+      </section>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,59 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Stephen L. Parker, DDS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
+      <main className="container py-4">
+        <Header title="Stephen L. Parker, DDS" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          General Dentist with 41 yrs of experience in Comprehensive Dentistry in the DFW Metroplex.
         </p>
+        <p>
+          By focusing on family, community, and integrity, I have earned lifelong generations of patients, many who followed me from my 27 years in Irving, to my Grapevine practice in 2007.
+        </p>
+        <p>
+          I sold my practice 2 years ago and have completed my contract in DFW.
+        </p>
+        <p>
+          I now live in NW Austin with my wife, Dina.
+        </p>
+
+        <h2>
+          Locum Tenens
+        </h2>
+
+        <p>
+          A current professional goal is to work as a Locum Tenens dentist with an upbeat team of fully trained auxiliary staff, helping me to be an effective "plug and play" dentist for Senior Dentists or Associates looking to take time off for C.E., Vacation, or unforeseen circumstances.
+        </p>
+
+        <h3 className="">
+          Restorative
+        </h3>
+
+        <h3 className="">
+          Prosthetics
+        </h3>
+
+        <h3 className="">
+          Esthetics
+        </h3>
+
+        <h3 className="">
+          Pediatrics
+        </h3>
+
+        <h3 className="">
+          Oral Surgery
+        </h3>
+
       </main>
 
       <Footer />
+
     </div>
   )
 }

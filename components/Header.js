723d14c5b37e97ac7b5566c135efc047107a0f56
Matthew Kivett <mkivett@gmail.com>
2023-12-28 00:46:53 -0600

    Content: Navigation Updated

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,15 @@ export default function Header({ title }) {
 
       <h3 className="m-0 text-uppercase text-primary">{title}</h3>
 
+      <nav className="section-nav nav small text-uppercase">
+          <a href="tel:8173128561" className="nav-link">
+              Call
+          </a>
+          <a href="mailto:driller1207@gmail.com" className="nav-link">
+              Email
+          </a>
+      </nav>
+
       <img class="logo" src="/atxdds-logo.svg" />
 
     </header>

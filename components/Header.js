40f9581c2bcfe421913b55c51116e1f748d280d4
Matthew Kivett <mkivett@gmail.com>
2023-12-27 21:41:03 -0600

    Component: Header
    Header updates

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
 export default function Header({ title }) {
   return (
-    <header className="section-header container">
-      <h1 className="title text-uppercase text-primary">{title}</h1>
+    <header className="section-header container-fluid">
+
+      <h3 className="m-0 text-uppercase text-primary">{title}</h3>
+
       <img class="logo" src="/atxdds-logo.svg" />
+
     </header>
   )  
 }

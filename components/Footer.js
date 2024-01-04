353843103199c972ce11b900a2867e81c6373619
Matthew Kivett <mkivett@gmail.com>
2024-01-04 01:21:03 -0600

    Content: Updates for Footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,21 @@ export default function Footer() {
     <>
       <section className="section-footer">
         <footer className="container">
-          <p className="small">
-            References Available On Request
-          </p>
-          <a href="tel:8173128561" className="small">
-              817-312-8561
-          </a>
+
+          <nav className="section-nav">
+              <a href="tel:8173128561" className="" title="Phone">
+                <i className="fa-solid fa-phone me-1"></i>817-312-8561
+              </a>
+              <br />
+              <a href="mailto:driller1207@gmail.com" className="" title="Email">
+                <i className="fa-solid fa-envelope me-1"></i>driller1207@gmail.com
+              </a>
+              <br />
+              <a href="#section-main" className="" title="Location: Central Texas">
+                <i className="fa-solid fa-location-dot me-1"></i>Central Texas
+              </a>
+          </nav>
+
           <img
             src="/images/stephen-parker--portrait.svg"
             alt="Stephen L Parker, DDS"

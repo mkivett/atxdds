6b1850905f4bae30d3d79b56faf94de2aa5e2153
Matthew Kivett <mkivett@gmail.com>
2024-01-04 03:11:50 -0600

    Layout: Section - Main
    Moved container to separate div within section-main

--- pages/index.js
+++ pages/index.js
@@ -15,37 +15,38 @@ export default function Home() {
 
       <Header title="Stephen L. Parker, DDS" />
 
-      <main id="section-main" className="section-main container py-4">
-        <p className="description">
-          General Dentist with 42 yrs of experience in Comprehensive Dentistry.
-        </p>
-        <p>
-          By focusing on family, community, and integrity, I have earned lifelong generations of patients.
-        </p>
-        <p>
-          I now live in Austin with my wife, Dina.
-        </p>
+      <main id="section-main" className="section-main">
+        <div className="container">
+          <p className="description">
+            General Dentist with 42 yrs of experience in Comprehensive Dentistry.
+          </p>
+          <p>
+            By focusing on family, community, and integrity, I have earned lifelong generations of patients.
+          </p>
+          <p>
+            I now live in Austin with my wife, Dina.
+          </p>
 
-        <header id="section-locum-tenens" className="d-flex align-items-center justify-content-between">
+          <header id="section-locum-tenens" className="d-flex align-items-center justify-content-between">
 
-          <h4 className="text-uppercase">
-            Locum Tenens
-          </h4>
+            <h4 className="text-uppercase">
+              Locum Tenens
+            </h4>
 
-          <h6 className="text-uppercase">
-            <i className="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Central Texas
-          </h6>
+            <h6 className="text-uppercase">
+              <i className="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Central Texas
+            </h6>
 
-        </header>
+          </header>
 
-        <p>
-          A current professional goal is to work as a Locum Tenens dentist with an upbeat team of fully trained auxiliary staff, helping me to be an effective "plug and play" dentist for Senior Dentists or Associates looking to take time off for C.E., Vacation, or unforeseen circumstances.
-        </p>
+          <p>
+            A current professional goal is to work as a Locum Tenens dentist with an upbeat team of fully trained auxiliary staff, helping me to be an effective "plug and play" dentist for Senior Dentists or Associates looking to take time off for C.E., Vacation, or unforeseen circumstances.
+          </p>
 
-        <a href="/steve-parker-resume.pdf" target="_blank" className="small text-uppercase">
-            View My Résumé
-        </a>
-
+          <a href="/steve-parker-resume.pdf" target="_blank" className="small text-uppercase">
+              View My Résumé
+          </a>
+        </div>
       </main>
 
       <Services />

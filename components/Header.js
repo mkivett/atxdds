a316bdfc44cbc717819fe8df15a7ceb0935e41e4
Matthew Kivett <mkivett@gmail.com>
2024-01-04 05:41:11 -0600

    Content: Header Markup Update

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header({ title }) {
       <section className="d-flex align-items-center">
         <a href="#" className="logo">
           <img className="logo_image" src="/atxdds-logo.svg" />
-          <h3 className="m-0 text-uppercase">{title}</h3>
+          <h3 className="logo_title m-0 text-uppercase">{title}</h3>
         </a>
       </section>
 

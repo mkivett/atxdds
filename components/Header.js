25e0665fc7c5189864fa4b666fe41bd475432ff4
Matthew Kivett <mkivett@gmail.com>
2024-01-04 01:20:34 -0600

    Content: Header Link Updated

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ export default function Header({ title }) {
           <a href="mailto:driller1207@gmail.com" className="nav-link" title="Email">
             <i className="fa-solid fa-envelope"></i>
           </a>
-          <a href="https://maps.app.goo.gl/tSu9R4YaaApXQ2oX6" target="_blank" className="nav-link" title="Location: Central Texas">
+          <a href="#section-main" className="nav-link" title="Location: Central Texas">
             <i className="fa-solid fa-location-dot"></i>
           </a>
       </nav>

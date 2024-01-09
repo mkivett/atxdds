5402a94bd4f9bbd3306716834acec16360a22c65
Matthew Kivett <mkivett@gmail.com>
2024-01-09 03:51:22 -0600

    Layout: Styling and Markup Changes

--- components/Services.js
+++ components/Services.js
@@ -1,89 +1,107 @@
 export default function Services() {
   return (
     <>
-      <section className="section-services container-fluid mb-4">
+    <section className="section-services container-fluid mb-4">
 
         <div className="row bg-gradient">
 
-          <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
-            <figure className="">
-              <h5 className="text-uppercase text-light">
-                Restorative
-              </h5>
-            </figure>
-          </div>
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Restorative
+                </h5>
+            </div>
 
-          <div className="col-sm-7 p-0">
-            <img className="services_image" src="/images/service-restorative.jpg" />
-          </div>
+            <div className="col-sm-7 p-0">
+                <img className="feature_image" src="/images/service-restorative.jpg" />
+            </div>
 
         </div>
 
         <div className="row bg-gradient">
 
-          <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
-            <figure className="">
-              <h5 className="text-uppercase text-light">
-                Prosthetics
-              </h5>
-            </figure>
-          </div>
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Prosthetics
+                </h5>
+            </div>
 
-          <div className="col-sm-7 p-0 order-sm-first">
-             <img className="services_image" src="/images/service-prosthetics.jpg" />
-          </div>
+            <div className="col-sm-7 p-0 order-sm-first">
+                <img className="feature_image" src="/images/service-prosthetics.jpg" />
+            </div>
 
         </div>
 
         <div className="row bg-gradient">
 
-          <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
-            <figure className="">
-              <h5 className="text-uppercase text-light">
-                Esthetics
-              </h5>
-            </figure>
-          </div>
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Esthetics
+                </h5>
+            </div>
 
-          <div className="col-sm-7 p-0">
-            <img className="services_image" src="/images/service-esthetics.jpg" />
-          </div>
+            <div className="col-sm-7 p-0">
+                <img className="feature_image" src="/images/service-esthetics.jpg" />
+            </div>
 
         </div>
 
         <div className="row bg-gradient">
 
-          <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
-            <figure className="">
-              <h5 className="text-uppercase text-light">
-                Pediatrics
-              </h5>
-            </figure>
-          </div>
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Pediatrics
+                </h5>
+            </div>
 
-          <div className="col-sm-7 p-0 order-sm-first">
-            <img className="services_image" src="/images/service-pediatrics.jpg" />
-          </div>
+            <div className="col-sm-7 p-0 order-sm-first">
+                <img className="feature_image" src="/images/service-pediatrics.jpg" />
+            </div>
 
         </div>
 
         <div className="row bg-gradient">
 
-          <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
-            <figure className="">
-              <h5 className="text-uppercase text-light">
-                Oral Surgery
-              </h5>
-            </figure>
-          </div>
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Sports Dentistry
+                </h5>
+            </div>
 
-          <div className="col-sm-7 p-0">
-            <img className="services_image" src="/images/service-oral-surgery.jpg" />
-          </div>
+            <div className="col-sm-7 p-0">
+                <img className="feature_image" src="/images/service-sports-dentistry.jpg" />
+            </div>
 
         </div>
 
-      </section>
+        <div className="row bg-gradient">
+
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Community
+                </h5>
+            </div>
+
+            <div className="col-sm-7 p-0 order-sm-first">
+                <img className="feature_image" src="/images/stephen-parker--flag-football.jpg" />
+            </div>
+
+        </div>
+
+        <div className="row bg-gradient">
+
+            <div className="col-sm-5 d-flex flex-column align-items-center justify-content-center p-5">
+                <h5 className="text-uppercase text-light">
+                    Experience
+                </h5>
+            </div>
+
+            <div className="col-sm-7 p-0">
+                <img className="feature_image" src="/images/stephen-parker--motorcycle.jpg" />
+            </div>
+
+        </div>
+
+    </section>
     </>
   )
 }
